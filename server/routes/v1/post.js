9c7a0a4f11a3a394f4582bcfbb0278a4da3acad2
Julia Donaldson <Julia@Julias-MacBook-Pro.local>
2017-10-19 15:03:19 -0700

    add authentication/authorization implementation to our posts routes

--- server/routes/v1/post.js
+++ server/routes/v1/post.js
@@ -1,4 +1,5 @@
 const autorouteJson = require('express-autoroute-json');
+const authmakerVerifyExpress = require('authmaker-verify-express');
 const models = require('../../../models').models;
 
 module.exports.autoroute = autorouteJson({
@@ -6,8 +7,31 @@ module.exports.autoroute = autorouteJson({
   resource: 'post', // this will be pluralised in the routes
 
   // default CRUD
-  find: {},
-  create: {},
-  update: {},
-  delete: {},
+  find: {},  // no authentication/authorization needed to view all posts
+  create: {
+    preMiddleware(req, res, next) {
+      // eslint-ignore-next-line no-param-reassign
+      req.body.data.attributes.author = req.user.id;
+      // assign the current user as the new post's author
+      next();
+    },
+    authentication: authmakerVerifyExpress.mongo(),
+    // user must be authenticated to create a new post
+  },
+  update: {
+    authentication: authmakerVerifyExpress.mongo(), // user must be authenticated to edit a post
+    authorisation(req) {
+      return {
+        author: req.user.id,
+      };
+    }, // user is only authorized to edit their own posts
+  },
+  delete: {
+    authentication: authmakerVerifyExpress.mongo(), // user must be authenticated to delete a post
+    authorisation(req) {
+      return {
+        author: req.user.id,
+      };
+    }, // user is only authorized to delete their own posts
+  },
 });
